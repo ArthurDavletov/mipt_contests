e92a3fd09e3b87d53cea4a29f4fe177ffc2ebc12
Arthur Davletov <ar.davletov2013@gmail.com>
2024-03-26 13:30:17 +0500

    Создал файлы

---
+++ task-cmake/A/preparing.py
@@ -0,0 +1,2 @@
+with open("task-cmake/A/index.h", "w", encoding="utf-8"):
+    pass
---
+++ task-cmake/C/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../B/lib.h"
+#include "../A/index.h"
+
+int main() {
+  std::cout << "Hello World" << std::endl;
+  return 0;
+}
