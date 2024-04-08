da470240dcbc9e4c075861de7b9e8ca6ce98c12e
Arthur Davletov <ar.davletov2013@gmail.com>
2024-04-08 20:18:33 +0500

    Покрытие тестами для первой части

--- task-testing-cpp/tests/01-simple-library/AddTestCase.cpp
+++ task-testing-cpp/tests/01-simple-library/AddTestCase.cpp
@@ -4,3 +4,10 @@
 
 #include "AddTestCase.h"
 #include "Functions.h"
+
+TEST(AddTestCase, AddNumbers) {
+    for (int i = -10; i < 10; i++) {
+        for (int j = -10; j < 10; j++)
+            ASSERT_EQ(Add(i, j), i + j);
+    }
+}
